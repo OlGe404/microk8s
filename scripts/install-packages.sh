70fa726d4c0432c008c56d3b6801422cf5a5ff4a
olge <olge@tux.com>
2021-11-12 10:58:57 +0100

    add ansible-galaxy collection install and python3 pip openshift and kubernetes module to resolve errors when using ansible 2.9.x with k8s module

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,49 +1,20 @@
 #!/bin/bash
 
-# this file is used to install all tools and packages needed to execute and test the ansible-playbook on ubuntu.
-# because microk8s is only available for ubuntu, it is the only distro considered here.
+# NOTE: this file is used to install the tools and packages 
+# needed to execute and test the playbook on ubuntu.
+# because microk8s is only available for ubuntu, 
+# it is the only case considered here.
+
+REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
 
 sudo apt-get update
-
-### docker setup
-if docker --version > /dev/null 2>&1 ; then
-    echo -e "\n======> Skipping Docker installation, because it already is installed. \n"
-else 
-    sudo apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release
-
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo \
-    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    sudo apt-get install -y \
-        docker-ce \
-        docker-ce-cli \
-        containerd.io
-
-    sudo groupadd docker && \
-    sudo usermod -aG docker $(whoami) && \
-    newgrp docker
-fi
-
-cat << EOF
-
-======> Docker location: $(which docker)
-======> Docker version: $(docker --version)
-
-EOF
-### docker setup
 
 ### ansible, vagrant and molecule setup
 sudo apt-get install -y \
     python3-pip \
     python3-venv \
-    libssl-dev
+    libssl-dev \
+    virtualbox
 
 if ansible --version > /dev/null 2>&1 ; then
     echo -e "\n======> Skipping Ansible installation, because it already is installed. \n"
@@ -51,19 +22,36 @@ else
     sudo apt-get install -y ansible
 fi
 
+ANSIBLE_VERSION=$(ansible --version | head -1 | awk '{print $2}')
+
+# with ansible 2.9.x, there is a bug where
+# newer version of kubernetes and openshift
+# won't work with the kubenetes.core module
+# so the working versions are fixated in that case
+if [[ $ANSIBLE_VERSION == *"2.9"* ]] ; then
+    python3 -m pip install \
+        "kubernetes==11.0.0" \
+        "openshift<0.12"
+else 
+    python3 -m pip install \
+        "kubernetes" \
+        "openshift"
+fi
+
+ansible-galaxy collection install -r $REPO_ROOT_DIR/requirements.yaml
+
 if vagrant --version > /dev/null 2>&1 ; then
     echo -e "\n======> Skipping Vagrant installation, because it already is installed. \n"
 else
     sudo apt-get install -y vagrant
 fi
-    
-ANSIBLE_VERSION=$(ansible --version | head -1 | awk '{print $2}')
 
 python3 -m venv molecule && source molecule/bin/activate
-python3 -m pip install wheel \
+python3 -m pip install \
+    wheel \
     ansible==$ANSIBLE_VERSION \
     python-vagrant \
-    testinfra \
+    pytest-testinfra \
     molecule \
     molecule-vagrant
 
@@ -76,6 +64,5 @@ cat << EOF
 ======> molecule version: $(molecule --version)
 
 EOF
-### ansible, vagrant and molecule setup
 
-sudo apt-get autoclean && sudo apt-get autoremove
+sudo apt-get autoclean && sudo apt-get autoremove -y
