52e60f67d126f373be5a64d76e927284880d47aa
olge <olge@tux.com>
2021-12-05 11:06:59 +0100

    fix paths for molecule venv after grouping ansible related files into ansible/ dir

--- scripts/molecule.sh
+++ scripts/molecule.sh
@@ -4,6 +4,8 @@ REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
 CMD=${1:-test}
 ROLE=${2:-microk8s}
 
-python3 -m venv $REPO_ROOT_DIR/molecule && source $REPO_ROOT_DIR/molecule/bin/activate
+cd $REPO_ROOT_DIR/ansible
 
-cd $REPO_ROOT_DIR/roles/$ROLE && molecule $CMD
+python3 -m venv molecule && source molecule/bin/activate
+
+cd $REPO_ROOT_DIR/ansible/roles/$ROLE && molecule $CMD
